f5ca7930f96fd2722fd31ef61cf90653f6be02b4
Kağan Temizkan <kaganntemizkan@gmail.com>
2024-09-12 19:56:08 +0300

    fixed fontWeight for android

--- app/login.tsx
+++ app/login.tsx
@@ -1,24 +1,33 @@
-import { useOAuth } from '@clerk/clerk-expo';
-import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { useRouter } from 'expo-router';
-import { Colors } from '@/constants/Colors';
-import { useState } from 'react';
-import { defaultStyles } from '@/constants/Styles';
+import { useOAuth } from "@clerk/clerk-expo";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import { useState } from "react";
+import { defaultStyles } from "@/constants/Styles";
 
 enum Strategy {
-  Google = 'oauth_google',
-  Apple = 'oauth_apple',
-  Facebook = 'oauth_facebook',
+  Google = "oauth_google",
+  Apple = "oauth_apple",
+  Facebook = "oauth_facebook",
 }
 
 const Page = () => {
   const router = useRouter();
 
-  const [email, setEmail] = useState('');
-  const { startOAuthFlow: googleAuth } = useOAuth({ strategy: 'oauth_google' });
-  const { startOAuthFlow: facebookAuth } = useOAuth({ strategy: 'oauth_facebook' });
-  const { startOAuthFlow: appleAuth } = useOAuth({ strategy: 'oauth_apple' });
+  const [email, setEmail] = useState("");
+  const { startOAuthFlow: googleAuth } = useOAuth({ strategy: "oauth_google" });
+  const { startOAuthFlow: facebookAuth } = useOAuth({
+    strategy: "oauth_facebook",
+  });
+  const { startOAuthFlow: appleAuth } = useOAuth({ strategy: "oauth_apple" });
 
   const onSelectAuth = async (strategy: Strategy) => {
     const selectedAuth = {
@@ -35,7 +44,7 @@ const Page = () => {
         router.back();
       }
     } catch (err) {
-      console.error('OAuth error', err);
+      console.error("OAuth error", err);
     }
   };
 
@@ -43,11 +52,17 @@ const Page = () => {
     <ScrollView style={styles.container}>
       <Text style={styles.header}>Log in or create an account</Text>
       <Text style={styles.subText}>
-        By continuing, you agree to the Terms of Sale, Terms of Service, and Privacy Policy.
+        By continuing, you agree to the Terms of Sale, Terms of Service, and
+        Privacy Policy.
       </Text>
 
       <Text style={styles.inputLabel}>Email address</Text>
-      <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={setEmail} />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
 
       <TouchableOpacity style={defaultStyles.btn}>
         <Text style={defaultStyles.btnText}>Continue with email</Text>
@@ -57,7 +72,7 @@ const Page = () => {
         <View
           style={{
             flex: 1,
-            borderBottomColor: 'black',
+            borderBottomColor: "black",
             borderBottomWidth: StyleSheet.hairlineWidth,
           }}
         />
@@ -65,24 +80,33 @@ const Page = () => {
         <View
           style={{
             flex: 1,
-            borderBottomColor: 'black',
+            borderBottomColor: "black",
             borderBottomWidth: StyleSheet.hairlineWidth,
           }}
         />
       </View>
 
       <View style={{ gap: 20 }}>
-        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Google)}>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Google)}
+        >
           <Ionicons name="logo-google" size={24} style={styles.btnIcon} />
           <Text style={styles.btnOutlineText}>Continue with Google</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Facebook)}>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Facebook)}
+        >
           <Ionicons name="logo-facebook" size={24} style={styles.btnIcon} />
           <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectAuth(Strategy.Apple)}>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Apple)}
+        >
           <Ionicons name="logo-apple" size={24} style={styles.btnIcon} />
           <Text style={styles.btnOutlineText}>Continue with Apple</Text>
         </TouchableOpacity>
@@ -93,26 +117,26 @@ const Page = () => {
 export default Page;
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     flex: 1,
     paddingHorizontal: 40,
   },
   header: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     paddingTop: 30,
     paddingBottom: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
   subText: {
     fontSize: 15,
-    color: '#4f4f4f',
-    textAlign: 'center',
+    color: "#4f4f4f",
+    textAlign: "center",
     marginBottom: 30,
   },
   input: {
     height: 50,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderWidth: 1,
     borderRadius: 4,
     paddingHorizontal: 10,
@@ -120,34 +144,34 @@ const styles = StyleSheet.create({
   },
   inputLabel: {
     paddingBottom: 5,
-    fontWeight: 500,
+    fontWeight: "500",
   },
   seperatorView: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 10,
-    alignItems: 'center',
+    alignItems: "center",
     marginVertical: 30,
   },
   seperator: {
-    fontFamily: 'mon-sb',
+    fontFamily: "mon-sb",
     color: Colors.light.gray,
     fontSize: 16,
   },
   btnOutline: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderWidth: 1,
-    borderColor: '#000',
+    borderColor: "#000",
     height: 50,
     borderRadius: 4,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
     paddingHorizontal: 10,
   },
   btnOutlineText: {
-    color: '#000',
+    color: "#000",
     fontSize: 16,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   btnIcon: {
     paddingRight: 10,
