393ad0f7be3a4396056cb30a5ae747bc52a65a7b
Kağan Temizkan <kaganntemizkan@gmail.com>
2024-09-16 12:20:54 +0300

    Update FirebaseConfig.ts

--- utils/FirebaseConfig.ts
+++ utils/FirebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCFPexeqHX8wSTE1l7KWLVdIoSaJWNJN8M",
-    authDomain: "wordle-clone-1bfad.firebaseapp.com",
-    projectId: "wordle-clone-1bfad",
-    storageBucket: "wordle-clone-1bfad.appspot.com",
-    messagingSenderId: "286973299858",
-    appId: "1:286973299858:web:ed6037ae6168528b3aea4b"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const FIREBASE_APP = initializeApp(firebaseConfig);
